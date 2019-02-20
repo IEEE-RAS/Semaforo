b38a1578fa358a035d041554621a5ead95284a96
Elvis Serafim <38743740+ElvisSerafim@users.noreply.github.com>
2019-02-20 20:02:43 -0300

    Add files via upload

---
+++ Semaforo.ino
@@ -0,0 +1,27 @@
+int ledVerde = 13;
+int ledAmarelo = 12;
+int ledVermelho = 11;
+
+void setup() {
+  pinMode(ledVerde, OUTPUT);
+  pinMode(ledAmarelo, OUTPUT);
+  pinMode(ledVermelho, OUTPUT);
+}
+
+void loop() {
+
+  digitalWrite(ledVerde, HIGH);
+  delay(4000);
+  digitalWrite(ledVerde, LOW);
+
+  digitalWrite(ledAmarelo, HIGH);
+  delay(2000);
+  digitalWrite(ledAmarelo, LOW);
+
+  digitalWrite(ledVermelho, HIGH);
+  delay(4000);
+  digitalWrite(ledVermelho, LOW);
+
+}
+
+
