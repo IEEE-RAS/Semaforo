8179a52b2aae5742f6b0ab0a3495535825e7514c
Elvis Serafim <38743740+ElvisSerafim@users.noreply.github.com>
2019-02-20 20:03:18 -0300

    Add files via upload

---
+++ Alerta_Buzzer.ino
@@ -0,0 +1,19 @@
+
+float seno;
+int frequencia;
+int buzzer = 9;
+ 
+void setup() {
+ pinMode(buzzer,OUTPUT);
+}
+ 
+void loop() {
+ for(int x=0;x<180;x++){
+  //converte graus para radiando e depois obtém o valor do seno
+  seno=(sin(x*3.1416/180));
+  //gera uma frequência a partir do valor do seno
+  frequencia = 2000+(int(seno*1000));
+  tone(buzzer,frequencia);
+  delay(2);
+ }
+}
